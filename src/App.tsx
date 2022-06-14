699d18a180c821fa5595f210a4a23e51efa2453e
Rodolfo Mariano <rodolfo@pop-os.localdomain>
2022-06-14 10:36:14 -0300

    feat: create modalActivities

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,62 @@
+import { ChangeEvent, ChangeEventHandler, useState } from "react"
+import { motion } from "framer-motion"
+import { PlusCircle, X } from 'phosphor-react'
+
 import styles from './App.module.scss'
 import './global.scss'
+
+import activityList from './utils/activityList.json'
 
 import { Header } from './components/Header'
 import { WarningMessage } from './components/WarningMessage'
 import { MealCard } from './components/MealCard'
 
-import { PlusCircle } from 'phosphor-react'
+interface Activity {
+  id: number
+  title: string
+  time: number
+  caloriesSpent: number
+}
 
 function App() {
+  const [genderOption, setGenderOption] = useState('')
+  const [bioType, setBioType] = useState('')
+  const [weight, setWeight] = useState(0)
+  const [stature, setStature] = useState(0)
+  const [age, setAge] = useState(0)
+  const [isOpenModalActivities, setIsOpenModalActivities] = useState(false)
+  const [closingModal, setClosingModal] = useState(false)
+
+  const [activitySelected, setActivitySelected] = useState<[] | any>([])
+  const [timeOfActivity, setTimeOfActivity] = useState(0)
+
+  const [myActivities, setMyActivities] = useState<Activity[]>([])
+
+  const kcalResultToActivity = activitySelected.length > 0 && (Number(activitySelected.split(',')[0]) * weight * timeOfActivity) / 60
+
+  console.log(kcalResultToActivity)
+
+  console.log(myActivities)
+
+  function handleAddActivity(activity: Activity) {
+    setMyActivities([...myActivities, activity])
+    closeModalActivities()
+  }
+
+  function openModalActivities() {
+    setIsOpenModalActivities(!isOpenModalActivities)
+  }
+
+  function closeModalActivities() {
+    setClosingModal(true)
+    setActivitySelected([])
+    setTimeOfActivity(0)
+
+    setTimeout(() => {
+      setIsOpenModalActivities(!isOpenModalActivities)
+      setClosingModal(false)
+    }, 900)
+  }
 
   return (
     <>
@@ -21,10 +70,20 @@ function App() {
             <div className={styles.personalInfoContent}>
               <div className={styles.genderAndBioType}>
                 <div className={styles.gender}>
-                  <strong>Genero:</strong>
+                  <strong>Gênero:</strong>
                   <div className={styles.genderOptions}>
-                    <button>Homen</button>
-                    <button>Mulher</button>
+                    <button
+                      onClick={() => setGenderOption('masculine')}
+                      className={genderOption === 'masculine' ? styles.active : ''}
+                    >
+                      Masculino
+                    </button>
+                    <button
+                      onClick={() => setGenderOption('feminine')}
+                      className={genderOption === 'feminine' ? styles.active : ''}
+                    >
+                      Feminino
+                    </button>
 
                   </div>
                 </div>
@@ -36,9 +95,24 @@ function App() {
                   </div>
 
                   <div className={styles.bioTypesContainer}>
-                    <button>Ectomorfo</button>
-                    <button>Mesomorfo</button>
-                    <button>Endomorfo</button>
+                    <button
+                      onClick={() => setBioType('ectomorfo')}
+                      className={bioType === 'ectomorfo' ? styles.active : ''}
+                    >
+                      Ectomorfo
+                    </button>
+                    <button
+                      onClick={() => setBioType('mesomorfo')}
+                      className={bioType === 'mesomorfo' ? styles.active : ''}
+                    >
+                      Mesomorfo
+                    </button>
+                    <button
+                      onClick={() => setBioType('endomorfo')}
+                      className={bioType === 'endomorfo' ? styles.active : ''}
+                    >
+                      Endomorfo
+                    </button>
                   </div>
                 </div>
               </div>
@@ -51,6 +125,8 @@ function App() {
                       type="number"
                       id="weight"
                       placeholder='ex: 73'
+                      value={weight > 0 ? weight : ''}
+                      onChange={(event: ChangeEvent<HTMLInputElement>) => setWeight(Number(event.target.value))}
                     />
                     <label htmlFor="weight">kg</label>
                   </div>
@@ -61,6 +137,8 @@ function App() {
                       type="number"
                       id="stature"
                       placeholder='ex: 173'
+                      value={stature > 0 ? stature : ''}
+                      onChange={(event: ChangeEvent<HTMLInputElement>) => setStature(Number(event.target.value))}
                     />
                     <label htmlFor="stature">cm</label>
                   </div>
@@ -71,6 +149,8 @@ function App() {
                       type="number"
                       id="age"
                       placeholder='35'
+                      value={age > 0 ? age : ''}
+                      onChange={(event: ChangeEvent<HTMLInputElement>) => setAge(Number(event.target.value))}
                     />
                     <label htmlFor="stature"></label>
                   </div>
@@ -78,7 +158,9 @@ function App() {
 
                 <div className={styles.physicalActivityButton}>
                   <strong>Atividades:</strong>
-                  <button>
+                  <button
+                    onClick={openModalActivities}
+                  >
                     <PlusCircle size={20} />
                     Add
                   </button>
@@ -95,7 +177,11 @@ function App() {
 
           </div>
 
-          <div className={styles.resultsContainer}>
+          <motion.div
+            // animate={{ scale: [0.5, 1] }}
+            // transition={{ duration: 0.5 }}
+            className={styles.resultsContainer}
+          >
 
             <WarningMessage
               iconPosition='center'
@@ -111,26 +197,39 @@ function App() {
             <div className={styles.ResultIMCContent}>
 
               <div className={styles.IMCContainer}>
-                <div className={styles.IMCInfo}>
+
+                <motion.div
+                  animate={{ scale: [0, 1], opacity: [0, 1] }}
+                  transition={{ duration: 0.5, delay: 0 }}
+                  className={styles.IMCInfo}
+                >
                   <div className={styles.IMCContent}>
                     <span>IMC</span>
                     <strong>24, 74</strong>
                   </div>
 
                   <p>Índice de Massa Corporal</p>
-                </div>
+                </motion.div>
 
-                <div className={styles.IMCInfo}>
+                <motion.div
+                  animate={{ scale: [0, 1], opacity: [0, 1] }}
+                  transition={{ duration: 0.5, delay: 0.25 }}
+                  className={styles.IMCInfo}
+                >
                   <div className={styles.IMCContent}>
                     <span>Gordura</span>
                     <strong>18.62%</strong>
                   </div>
 
                   <p>Gordura Corporal Corporal</p>
-                </div>
+                </motion.div>
               </div>
 
-              <div className={styles.IMCTableOfResults}>
+              <motion.div
+                animate={{ scale: [0, 1], opacity: [0, 1] }}
+                transition={{ duration: 0.5, delay: 0.5 }}
+                className={styles.IMCTableOfResults}
+              >
                 <table>
                   <tr>
                     <th>Condição</th>
@@ -158,51 +257,78 @@ function App() {
                     <td>{'> 89.8 Kg'}</td>
                   </tr>
                 </table>
-              </div>
+              </motion.div>
 
-              <div className={styles.warningMessageContainer}>
+              <motion.div
+                animate={{ scale: [0, 1], opacity: [0, 1] }}
+                transition={{ duration: 0.5, delay: 0.75 }}
+                className={styles.warningMessageContainer}
+              >
                 <WarningMessage
                   iconPosition='start'
                   position='center'
                   message='Nota: Os resultados aqui apresentados podem não indicar a sua realidade, uma vez que a tabela é baseada em individuos “normais”. Por exemplo: se você for um fisioculturista, irá dar IMC auto, mas isso não quer dizer que está obeso. Procure um especialista para ter resultados mais realistas.'
                 />
-              </div>
+              </motion.div>
             </div>
 
             <div className={styles.ResultGEBContainer}>
 
               <div className={styles.GEBContainer}>
-                <div className={styles.GEBInfo}>
+
+                <motion.div
+                  animate={{ scale: [0, 1], opacity: [0, 1] }}
+                  transition={{ duration: 0.5, delay: 1 }}
+                  className={styles.GEBInfo}
+                >
                   <div className={styles.GEBContent}>
                     <span>GEB</span>
                     <strong>2.600kcal</strong>
                   </div>
 
                   <p>Gasto Energético Basal</p>
-                </div>
+                </motion.div>
 
-                <div className={styles.GEBInfo}>
+                <motion.div
+                  animate={{ scale: [0, 1], opacity: [0, 1] }}
+                  transition={{ duration: 0.5, delay: 1.25 }}
+                  className={styles.GEBInfo}
+                >
                   <div className={styles.GEBContent}>
                     <span>Exercícios</span>
                     <strong>700kcal</strong>
                   </div>
 
                   <p>Gastos em Exercícios</p>
-                </div>
+                </motion.div>
 
-                <span>=</span>
+                <motion.span
+                  animate={{ scale: [0, 1], opacity: [0, 1] }}
+                  transition={{ duration: 0.5, delay: 1.5 }}
 
-                <div className={styles.GEBInfo}>
+                >
+                  =
+                </motion.span>
+
+                <motion.div
+                  animate={{ scale: [0, 1], opacity: [0, 1] }}
+                  transition={{ duration: 0.5, delay: 1.75 }}
+                  className={styles.GEBInfo}
+                >
                   <div className={styles.GEBContent}>
                     <span>GET</span>
                     <strong>3.300kcal</strong>
                   </div>
 
                   <p>Gasto Energético Total</p>
-                </div>
+                </motion.div>
               </div>
 
-              <div className={styles.microsPerDayToConsumeContainer}>
+              <motion.div
+                animate={{ scale: [0, 1], opacity: [0, 1] }}
+                transition={{ duration: 0.5, delay: 2 }}
+                className={styles.microsPerDayToConsumeContainer}
+              >
 
                 <div className={styles.objectiveTypesContainer}>
                   <button className={styles.objectiveTypeButton}>
@@ -245,18 +371,26 @@ function App() {
                     </div>
                   </div>
                 </div>
-              </div>
+              </motion.div>
 
-              <div className={styles.warningMessageContainer}>
+              <motion.div
+                animate={{ scale: [0, 1], opacity: [0, 1] }}
+                transition={{ duration: 0.5, delay: 2.25 }}
+                className={styles.warningMessageContainer}
+              >
                 <WarningMessage
                   iconPosition='start'
                   position='center'
-                  message='Nota: Os resultados aqui apresentados podem não indicar a sua realidade, uma vez que a tabela é baseada em individuos “normais”. Por exemplo: se você for um fisioculturista, irá dar IMC auto, mas isso não quer dizer que está obeso. Procure um especialista para ter resultados mais realistas.'
+                  message='Nota: Gastos energéticos durante taréfas cotidianas, como: lavar louça, limpar a casa, etc, não estão inclusos no calculo!'
                 />
-              </div>
+              </motion.div>
             </div>
 
-            <div className={styles.kcalSuggestionPerMeal}>
+            <motion.div
+              animate={{ scale: [0, 1], opacity: [0, 1] }}
+              transition={{ duration: 0.5, delay: 2.5 }}
+              className={styles.kcalSuggestionPerMeal}
+            >
               <div className={styles.suggestionHeader}>
                 <strong>
                   Divisão das calorias por refeição
@@ -278,11 +412,89 @@ function App() {
                   <strong>100%</strong> das calorias distribuidas
                 </span>
               </div>
-            </div>
-          </div>
+            </motion.div>
+          </motion.div>
 
         </div>
       </main>
+
+      {isOpenModalActivities && (
+        <div className={styles.modalActivities}>
+          <div className={styles.modalActivitiesBackground}>
+            <motion.div
+              animate={
+                closingModal
+                  ? { scale: [1, 1.2, 0], }
+                  : { scale: [0, 1.2, 1], }
+              }
+              transition={{ duration: 0.75, delay: 0 }}
+              className={styles.modalActivitiesContent}
+            >
+              <div className={styles.modalHeader}>
+                <strong>
+                  Atividades
+                </strong>
+                <button
+                  onClick={closeModalActivities}
+                >
+                  <X size={16} />
+                </button>
+              </div>
+
+              <div className={styles.activitiesOptions}>
+                <strong>Selecione uma atividade</strong>
+                <select
+                  name="" id=""
+                  onChange={(event: ChangeEvent<HTMLSelectElement>) => setActivitySelected(event.target.value)}
+                >
+                  {activityList.map((activity => (
+                    <option key={activity.id} value={[String(activity.met), activity.title]}>{activity.title}</option>
+                  )))}
+
+                </select>
+
+                <div className={styles.timeTraining}>
+                  <strong>Treinou quanto tempo?</strong>
+                  <div className={styles.timeInput}>
+                    <input
+                      type="number"
+                      id='time'
+                      placeholder={timeOfActivity > 0 ? String(timeOfActivity) : 'ex: 60'}
+                      onChange={(event: ChangeEvent<HTMLInputElement>) => setTimeOfActivity(Number(event.target.value))}
+                    />
+                    <label htmlFor="time">min</label>
+                  </div>
+                </div>
+              </div>
+
+              <div className={styles.caloriesSpent}>
+                <strong>{kcalResultToActivity ? (kcalResultToActivity).toFixed(2).replace('.', ',') : 0}kcal</strong><span> gastas</span>
+              </div>
+
+              <footer>
+                <button
+                  onClick={closeModalActivities}
+                >
+                  Cancelar
+                </button>
+                <button
+                  onClick={() => {
+                    handleAddActivity({
+                      id: myActivities.length + 1,
+                      title: activitySelected.split(',')[1],
+                      time: timeOfActivity,
+                      caloriesSpent: kcalResultToActivity ? kcalResultToActivity : 0
+                    })
+                  }}
+                >
+                  Confirmar
+                </button>
+              </footer>
+            </motion.div>
+          </div>
+        </div>
+
+      )}
     </>
   )
 }
